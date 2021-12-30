ec84a2de5c5992b81c337673e6bebb5fb64609b2
PoojithaR26 <poojitha.ramesh.2019.csbs@rajalakshmi.edu.in>
2021-12-30 23:31:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
 
-function App() {
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/Loginform";
+import "./App.css";
+
+const projectID = "7f955663-6b7b-4c56-adb5-a81b3457d715";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="7f955663-6b7b-4c56-adb5-a81b3457d715"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} //own components for the chat
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
